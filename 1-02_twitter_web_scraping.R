bf23926ed8273a07a05e2e58fb2341ad62ae95d4
Johann Wagner <103425902+johann-wagner@users.noreply.github.com>
2023-07-20 16:55:02 +1000

    Create 1-02_twitter_web_scraping.R

---
+++ 1-02_twitter_web_scraping.R
@@ -0,0 +1,11 @@
+### Setup and Configuration ----------------------------------------------------
+source(here::here('0-00_setup_and_configuration.R'))
+library(twitteR)
+
+
+### Twitter API Setup ----------------------------------------------------------
+
+source(here::here('api_keys.R'))
+
+consumer_token
+#setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
